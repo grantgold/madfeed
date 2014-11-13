240e667cf25654530b0132536496b7f9daa2bf55
grantgold <grantagold@gmail.com>
2014-11-13 18:02:17 -0500

    Adjusts Navbar

--- inc/navbar.php
+++ inc/navbar.php
@@ -8,14 +8,17 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">MADfeed</a>
+      <a class="navbar-brand" href="#"><img src="img/icons/logo-madfeed.jpg" height="80px" width="80px"></a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
+        <li><a href="#">Watch</a></li>
+        <li><a href="#">Read</a></li>
+        <li><a href="#">Attend</a></li>
+        <li><a href="#">Symposiums</a></li>
+        <li><a href="#">About</a></li>
         <li><a href="#"><i class="fa fa-search"></i></a></li>
       </ul>
     </div><!-- /.navbar-collapse -->
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <section id="blog">
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-12 col-sm-11 col-sm-offset-1">
+			<div class="col-xs-12 col-sm-11">
 			<p>!Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
