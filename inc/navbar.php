2ca5e765d70ee8c5e57aa15745b9069f50f4a6ae
grantgold <grantagold@gmail.com>
2014-11-15 12:24:59 -0500

    nav

--- inc/navbar.php
+++ inc/navbar.php
@@ -9,7 +9,7 @@
         <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand" href="#"><img src="img/icons/logo-madfeed.jpg" height="80px" width="80px"></a>
-    </div>
+      </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
