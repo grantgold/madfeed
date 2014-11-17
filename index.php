0bb2460e3464a804004c8fa0f5df7269272f2e8a
grantgold <grantagold@gmail.com>
2014-11-17 13:32:37 -0500

    fixes index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include('header.php'); ?>
+
+<section id="blog">
+	<div class="container">
+		<div class="row">
+			<?php include( INC . 'topics.php' ); ?>
+		</div>
+	</div>
+	<div class="row">
+
+			<?php include( INC . 'carousel.php' ); ?>
+
+	</div>
+	<div class="container">
+		
+		<?php include( INC . 'latest-reads.php' ); ?>
+
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
