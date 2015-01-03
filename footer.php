b8eb1b0feddc3932c6ec0cbd28695ef34f760149
grantgold <grantagold@gmail.com>
2015-01-03 17:55:35 -0500

    Fixes Footer Collapsing

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 	<section id="footer">
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-6 col-md-2">
+				<div class="col-xs-4 col-sm-3 col-md-2">
 					<a class="navbar-brand" href="#"><div class="footer-brand-text"><div>MAD</div></div></a>
 				</div>
-				<div class="col-xs-6 col-md-2 small-top-btm-padding"><b>This Site</b>
+				<div class="col-xs-4 col-sm-3 col-md-2 small-top-btm-padding"><b>This Site</b>
 					<ul class="list-unstyled">
 					<li><br></li>
 					<li><a href="/videos.php">WATCH</a></li>
@@ -12,27 +12,27 @@
 			        <li><a href="#">ATTEND</a></li>
 			        <li><a href="#">ABOUT</a></li></ul>
 			    </div>
-			    <div class="col-xs-6 col-md-2 small-top-btm-padding"><b>Follow MAD</b>
+			    <div class="col-xs-4 col-sm-3 col-md-2 small-top-btm-padding"><b>Follow MAD</b>
 					<ul class="list-unstyled">
 					<li><br></li>
 					<li><a href="https://twitter.com/TheMADFeed" target="_blank">Twitter</a></li>
 			        <li><a href="https://www.facebook.com/MADSymposium" target="_blank">Facebook</a></li>
 			        <li><a href="http://instagram.com/madsymposium" target="_blank">Instagram</a></li></ul>
 			    </div>
-			    <div class="col-xs-6 col-md-2 small-top-btm-padding"><b>Media</b>
+			    <div class="col-xs-4 col-sm-3 col-md-2 small-top-btm-padding"><b>Media</b>
 					<ul class="list-unstyled">
 					<li><br></li>
 					<li><a href="http://vimeo.com/madsymposium" target="_blank">Vimeo</a></li>
 			        <li><a href="http://www.youtube.com/channel/UCFyscqROV8WR3S1-IzOEG4A" target="_blank">YouTube</a></li>
 			        <li><a href="https://itunes.apple.com/dk/podcast/mad-symposium/id602624887?mt=2&ign-mpt=uo=4" target="_blank">iTunes</a></li></ul>
 			    </div>
-			    <div class="col-xs-6 col-md-2 small-top-btm-padding"><b>Info</b>
+			    <div class="col-xs-4 col-sm-3 col-md-2 small-top-btm-padding"><b>Info</b>
 					<ul class="list-unstyled">
 					<li><br></li>
 					<li><a href="#">Contact</a></li>
 			        <li><a href="#">Press</a></li></ul>
 			    </div>
-		    	<div class="col-xs-6 col-md-2 small-top-btm-padding">
+		    	<div class="col-xs-4 col-sm-3 col-md-2 small-top-btm-padding">
 		    	<b>Get MAD email</b>
 		    	<p class="subtitle medium-top-btm-padding">Subscribe to receive email notifications about news or events</p>
 				<a href="http://eepurl.com/utvuX" target="_blank"><button class="btn btn-default" type="button" >Subscribe <i class="fa fa-angle-right"></i></button></a>
