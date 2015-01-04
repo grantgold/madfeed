63cdfa69af4ae6b4f9c11b842e77aef1496d7eaa
Jeremy Zilar <jeremyzilar@gmail.com>
2015-01-04 16:47:48 -0500

    adding related video

---
+++ functions/related-video.php
@@ -0,0 +1,59 @@
+<?php
+add_action( 'add_meta_boxes', 'related_video_add' );
+function related_video_add() {
+  add_meta_box( 'related-video-id', 'Related Video', 'related_video', 'video', 'normal', 'high' );
+}
+
+function related_video( $post ) {
+  $values = get_post_custom( $post->ID );
+  $url = isset( $values['related_video_url'] ) ? esc_attr( $values['related_video_url'][0] ) : '';
+  wp_nonce_field( 'my_meta_box_nonce', 'meta_box_nonce' );?>
+
+  <style type="text/css" media="screen">
+    #related_video_box{}
+    #related_video_box label,
+    #related_video_box input,
+    #related_video_box small{}
+    #related_video_box label{
+      padding:0 2px;
+    }
+    #related_video_url{
+      width:100%;
+    }
+    #related_video_box small{
+      padding:0 3px;
+      color:#999;
+    }
+  </style>
+  <div id="related_video_box">
+    <p>
+      <label for="related_video_url">URL</label><br />
+      <input type="text" name="related_video_url" id="related_video_url" value="<?php echo $url; ?>" /><br />
+      <small>e.g. http://nytimes.com/<?php echo date('Y'); ?>/<?php echo date('m'); ?>/<?php echo date('d'); ?>/the-future-is-bright</small>
+    </p>
+  </div><!-- #related_video_box -->
+  
+  <?php }
+
+add_action( 'save_post', 'cd_meta_box_save' );
+function cd_meta_box_save( $post_id ) {
+  // Bail if we're doing an auto save
+  if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+  
+  // if our nonce isn't there, or we can't verify it, bail
+  if( !isset( $_POST['meta_box_nonce'] ) || !wp_verify_nonce( $_POST['meta_box_nonce'], 'my_meta_box_nonce' ) ) return;
+  
+  // if our current user can't edit this post, bail
+  if( !current_user_can( 'edit_post' ) ) return;
+  
+  // now we can actually save the data
+  $allowed = array( 
+    'a' => array( // on allow a tags
+      'href' => array() // and those anchords can only have href attribute
+    )
+  );
+  
+  // Probably a good idea to make sure your data is set
+  if( isset( $_POST['related_video_url'] ) )
+    update_post_meta( $post_id, 'related_video_url', wp_kses( $_POST['related_video_url'], $allowed ));
+}
--- functions/videos.php
+++ functions/videos.php
@@ -1,38 +1,36 @@
 <?php
-
 // REGISTER THE Video POST TYPE
 add_action('init', 'cptui_register_my_cpt_video');
 function cptui_register_my_cpt_video() {
-register_post_type('video', array(
-'label' => 'Videos',
-'description' => '',
-'public' => true,
-'show_ui' => true,
-'show_in_menu' => true,
-'capability_type' => 'post',
-'map_meta_cap' => true,
-'hierarchical' => false,
-'rewrite' => array('slug' => 'video', 'with_front' => true),
-'query_var' => true,
-'has_archive' => true,
-'supports' => array('title','editor','excerpt','revisions','thumbnail','author'),
-'taxonomies' => array('category','post_tag'),
-'labels' => array (
-  'name' => 'Videos',
-  'singular_name' => 'Video',
-  'menu_name' => 'Videos',
-  'add_new' => 'Add Video',
-  'add_new_item' => 'Add New Video',
-  'edit' => 'Edit',
-  'edit_item' => 'Edit Video',
-  'new_item' => 'New Video',
-  'view' => 'View Video',
-  'view_item' => 'View Video',
-  'search_items' => 'Search Videos',
-  'not_found' => 'No Videos Found',
-  'not_found_in_trash' => 'No Videos Found in Trash',
-  'parent' => 'Parent Video',
-)
-) ); }
-
-?>
+  register_post_type('video', array(
+  'label' => 'Videos',
+  'description' => '',
+  'public' => true,
+  'show_ui' => true,
+  'show_in_menu' => true,
+  'capability_type' => 'post',
+  'map_meta_cap' => true,
+  'hierarchical' => false,
+  'rewrite' => array('slug' => 'video', 'with_front' => true),
+  'query_var' => true,
+  'has_archive' => true,
+  'supports' => array('title','editor','excerpt','revisions','thumbnail','author'),
+  'taxonomies' => array('category','post_tag'),
+  'labels' => array (
+    'name' => 'Videos',
+    'singular_name' => 'Video',
+    'menu_name' => 'Videos',
+    'add_new' => 'Add Video',
+    'add_new_item' => 'Add New Video',
+    'edit' => 'Edit',
+    'edit_item' => 'Edit Video',
+    'new_item' => 'New Video',
+    'view' => 'View Video',
+    'view_item' => 'View Video',
+    'search_items' => 'Search Videos',
+    'not_found' => 'No Videos Found',
+    'not_found_in_trash' => 'No Videos Found in Trash',
+    'parent' => 'Parent Video',
+  )
+  ));
+}
