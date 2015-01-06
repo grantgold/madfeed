f5fc726b3b82f7509896d9c0c65345af814888f0
grantgold <grantagold@gmail.com>
2015-01-05 23:29:20 -0500

    Video Featured Image links and year links

--- content-videos.php
+++ content-videos.php
@@ -1,10 +1,10 @@
 <div class="col-sm-6 col-md-3">
   <div class="small-top-btm-padding">
-    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+    <a class="video-feature-image" href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
     <p class="subtitle"><a href="#">Christian Puglisi</a></p>
     <div class="video-title">
       <h4><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h4>
     </div>
-    <p class="byline"><a href="#">2014</a></p>
+    <p class="byline"><a href="<?php echo get_year_link(''); ?>"><?php the_time('Y'); ?></a></p>
   </div>
 </div>
--- inc/navbar.php
+++ inc/navbar.php
@@ -8,15 +8,15 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand hidden-xs hidden-sm" href="<?php get_template_directory(); ?>"><div class="circle-text"><div>MAD</div></div></a>
-      <a class="navbar-brand hidden-md hidden-lg" href="<?php get_template_directory(); ?>"><div class="brand-text"><div>MAD</div></div></a>
+      <a class="navbar-brand hidden-xs hidden-sm" href="<?php echo get_site_url(); ?>"><div class="circle-text"><div>MAD</div></div></a>
+      <a class="navbar-brand hidden-md hidden-lg" href="<?php echo get_site_url(); ?>"><div class="brand-text"><div>MAD</div></div></a>
       <div id="mission" class="hidden-xs">A community of chefs, cooks and farmers with an appetite for knowledge.</div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="/videos.php">Watch</a>
-        <a href="/reads.php">Read</a>
+        <li><a href="<?php echo get_site_url(); ?>/video">Watch</a>
+        <a href="#">Read</a>
         <a href="#">Attend</a>
         <a href="#">About</a></li>
        <!-- <li><a href="#"><i class="fa fa-search"></i></a></li> -->
