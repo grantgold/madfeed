5e9644427bc4fa2efaf6385156358f018633afa5
grantgold <grantagold@gmail.com>
2015-01-15 09:43:31 -0500

    video posts for mobile

--- content-video.php
+++ content-video.php
@@ -4,7 +4,7 @@
       <div class="col-xs-12 col-sm-10 col-sm-offset-1">
         <div class="entry-media embed-responsive embed-responsive-4by3">
 
-          <iframe class="embed-responsive-item" src="//player.vimeo.com/video/<?php echo vimeo_id();?>?portrait=0&byline=0&title=0&color=006633" width="100%" height="500" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+          <iframe class="embed-responsive-item" src="//player.vimeo.com/video/<?php echo vimeo_id();?>?portrait=0&byline=0&title=0&color=006633" width="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 
         </div>
       </div>
@@ -17,8 +17,12 @@
     <div class="row">
 
       <!-- Profile Image -->
-      <div class="col-xs-12 col-sm-2 profile-image">
-        <img class="img-responsive" src="http://placehold.it/200x200" />
+      <div class="col-xs-6 col-sm-2 profile-image">
+          <img class="img-responsive" src="http://placehold.it/200x200" />
+      </div>
+      <div class="col-xs-6 hidden-sm hidden-md hidden-lg profile">
+        <div class="profile-name">Daniel Klein &amp; Mirra Fine</div>
+        <div class="profile-title">Psychologist, USA</div>
       </div>
 
       <!-- Content -->
@@ -29,6 +33,8 @@
           <div class="entry-hed">
             <h1><?php the_title(); ?></h1>
           </div>
+
+          <div class="event-and-date">MAD • 2014</div>
 
           <div class="entry-content blogpost">
             <?php the_content(); ?>
