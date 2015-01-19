26d4e9b111d3e5eee88fbbadea3f26ca0cf84d83
Jeremy Zilar <jeremyzilar@gmail.com>
2015-01-19 12:10:09 -0500

    about page custom template

---
+++ about.php
@@ -0,0 +1,17 @@
+<?php
+/* Template Name: About */
+get_header();?>
+
+<section id="blog">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-11 col-sm-offset-1">
+        <h5>About Custom Template</h5>
+        <?php loop(); ?>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<?php include('footer.php'); ?>
