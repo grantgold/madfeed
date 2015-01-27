bb26761ede0c9c6019a07a7afac1e172e9084cf8
grantgold <grantagold@gmail.com>
2015-01-26 20:20:51 -0500

    Adds contributors to videos archive

--- content-videos.php
+++ content-videos.php
@@ -1,7 +1,7 @@
 <div class="col-sm-6 col-md-3">
   <div class="small-top-btm-padding">
     <a class="feature-image" href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-    <p class="subtitle"><a href="#">Christian Puglisi</a></p>
+    <p class="subtitle"><?php echo madfeed_contributors() ?></p>
     <div class="video-title">
       <h4><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h4>
     </div>
