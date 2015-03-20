9b489b5893aa7a0c9c34e5f4c38e664d9c61fb9d
grantgold <grantagold@gmail.com>
2015-03-20 17:10:31 -0400

    About page updates

--- page-about.php
+++ page-about.php
@@ -6,9 +6,9 @@
 <section id="about" class="fadein">
   <div class="container">
     <div class="row">
-      <div class="col-xs-12">
-        
-        <div class="entry-title about-heavy"><u>MAD</u> : <span class="about-light">(taken from the Danish word for “food”)</span> is a not-for-pro&#xFB01;t organization with the mission to build a future where there is no con&#xFB02;ict between a better meal and a better world.</div>
+      <div class="col-xs-12 col-sm-11 col-sm-offset-1">
+        <div class="entry-title about-heavy">For a better meal.</div>
+        <div class="entry-title about-heavy"><u>MAD</u>: <span class="about-light">(taken from the Danish word for “food”)</span> is a not-for-pro&#xFB01;t organization with the mission to build a better world through a better meal.</div>
         <hr>
         <div class="col-xs-4 medium-top-btm-padding img-responsive about"><img src="<?php echo get_template_directory_uri(); ?>/img/illo/chef.jpg"></div>
         <div class="col-xs-8 medium-top-btm-padding about-medium">Sitting at the intersection of cooking, culture, science, policy, and food production, MAD’s vision is to create an interdisciplinary conversation about sustainability and deliciousness. <i>Embracing the forces that power today's most influential restaurants—creativity, collaboration, research, and innovation—MAD serves as an international platform for talking and thinking about topics at the forefront of cooking.</i> From inspiring collaboration between chefs, producers, and academics to performing research into various culinary questions; <b>MAD is a place where new knowledge is produced and shared without restrictions, and with the commitment to taking promising ideas from theory and put them into practice.</b>
