db061544e0bf269bb9b141845f1c61aa7c4ce0c7
grantgold <grantagold@gmail.com>
2015-04-07 19:48:56 -0400

    Updates related posts logic
    Also adds tangentially related posts for lack of posts.

--- inc/related-posts.php
+++ inc/related-posts.php
@@ -45,7 +45,35 @@
 						$i++;
 						}
 					} else {
-						// get_template_part( 'content' );
+						$secondargs=array(
+						'post_type' => array('post','video'),
+						'post__not_in' => array($post->ID),
+						'post__not_in' => $do_not_duplicate,
+						'orderby' => 'rand',
+						'posts_per_page'=> 4, // Number of related posts that will be displayed.
+						'caller_get_posts'=>1,
+						'orderby'=>'rand' // Randomize the posts
+						);
+						$the_query = new WP_Query($secondargs);
+						if ( $the_query->have_posts() ) {
+							while ( $the_query->have_posts() ) {
+								$the_query->the_post(); ?>
+								<?php
+									$type = get_post_type( get_the_ID() );
+									if ($type == 'post') {
+										get_template_part('content', 'posts'); // uses content-posts.php
+									} else {
+										get_template_part('content', 'videos');  // uses content-videos.php
+									}
+								
+									if ($i % 4 == 0){
+										echo "</div></div>";
+										echo "<div class='row'><div class='col-xs-12'>";
+									}
+
+							$i++;
+							} 
+						}else {}
 					}
 				?>
 			</div>
