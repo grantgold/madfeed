1efcb2f4dff18c80df2427fe8dcc7c702ce594ae
grantgold <grantagold@gmail.com>
2015-04-13 18:08:03 -0400

    Video fix

--- content-videos.php
+++ content-videos.php
@@ -1,13 +1,13 @@
 <div class="col-sm-6 col-md-3">
   <div class="small-top-btm-padding">
 		<?php if (vimeo_id() == TRUE) { 
-			$imgid = vimeo_id();
-      $img_url = $image_json->$imgid['image_url'];
-			// $hash = unserialize(file_get_contents("http://vimeo.com/api/v2/video/$imgid.php"));
+			// $imgid = vimeo_id();
+   //    $img_url = $image_json->$imgid['image_url'];
+			$hash = unserialize(file_get_contents("http://vimeo.com/api/v2/video/$imgid.php"));
 		?>
         <div class="feature-image img-responsive">
-          <!-- <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><img src="<?php echo $hash[0]['thumbnail_large']; ?>"></a> -->
-          <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><img src="<?php echo $img_url ?>"></a>
+          <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><img src="<?php echo $hash[0]['thumbnail_large']; ?>"></a>
+<!--           <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><img src="<?php echo $img_url ?>"></a> -->
         </div>
   <?php } else { ?>
   		<div class="feature-image img-responsive"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('medium'); ?></a></div>
