e5cebdce0d50cb5051981a225f9c4fe1536fbdcc
grantgold <grantagold@gmail.com>
2015-04-13 18:31:14 -0400

    More video fixes

--- content-carousel.php
+++ content-carousel.php
@@ -1,13 +1,8 @@
 <div>
 	<div class="carousel-gradient">
-<!-- 		<?php if (vimeo_id() == TRUE) { 
-			$imgid = vimeo_id();
-			$hash = unserialize(file_get_contents("http://vimeo.com/api/v2/video/$imgid.php"));
-		?>
-			<a class="owl-item carousel-item img-resposive" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><img src="<?php echo $hash[0]['thumbnail_large']; ?>"></a>
-		<?php } else { ?> -->
+
 		  	<a class="owl-item carousel-item img-responsive" href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-<!-- 		<?php } ?> -->
+
 	</div>
 	<div class="carousel-text">
 		<?php
--- content-videos.php
+++ content-videos.php
@@ -1,16 +1,8 @@
 <div class="col-sm-6 col-md-3">
   <div class="small-top-btm-padding">
-<!-- 		<?php if (vimeo_id() == TRUE) { 
-		 $imgid = vimeo_id();
-			$hash = unserialize(file_get_contents("http://vimeo.com/api/v2/video/$imgid.php"));
-		?>
-        <div class="feature-image img-responsive">
-          <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><img src="<?php echo $hash[0]['thumbnail_large']; ?>"></a>
 
-        </div>
-  <?php } else { ?> -->
   		<div class="feature-image img-responsive"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail('medium'); ?></a></div>
-<!--   <?php } ?> -->
+
     <div class="subtitle"></div>
     <div class="video-title">
       <h4><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><i class="fa fa-play-circle"></i> &nbsp;<?php the_title(); ?></a></h4>
