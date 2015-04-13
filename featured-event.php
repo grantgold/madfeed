a645e4df56f73e625cc21079c348b4b98809cbdd
grantgold <grantagold@gmail.com>
2015-04-13 11:15:01 -0400

    Space styling

--- featured-event.php
+++ featured-event.php
@@ -7,20 +7,7 @@
 					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_post_thumbnail(); ?></a>
 				</div>
 			</div> 
-		<?php } else { }
-
-		if (is_home()) { ?>
-		<div class="col-xs-12 col-sm-6">
-			<p class="subtitle no-padding"><?php echo get_event_location() ?></p>
-			<h1 class="featured-event"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
-			<p class="byline"><?php echo get_event_date() ?></p>
-			<?php the_excerpt(); ?>
-			<div class="medium-top-btm-padding">
-				<a class="btn btn-primary button-list" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">Event Details <i class="fa fa-angle-right"></i></a>
-				<!-- <a class="btn btn-primary button-list" href="#">Sign Up <i class="fa fa-angle-right"></i></a> -->
-			</div>
-		</div>
-		<?php } else { ?>
+		<?php } else { } ?>
 		<div class="col-xs-12 col-sm-6">
 			<p class="subtitle"><?php echo get_event_location() ?></p>
 			<h1 class="featured-event"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h1>
@@ -30,6 +17,6 @@
 				<a class="btn btn-primary button-list" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">Event Details <i class="fa fa-angle-right"></i></a>
 				<!-- <a class="btn btn-primary button-list" href="#">Sign Up <i class="fa fa-angle-right"></i></a> -->
 			</div>
-		</div> <?php } ?>
+		</div>
 	</div>
 </div>
