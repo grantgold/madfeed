ed17aa4ee1df6103970f3288204af42a45926c37
grantgold <grantagold@gmail.com>
2015-04-13 18:03:43 -0400

    Updates to donation link

--- header.php
+++ header.php
@@ -12,6 +12,11 @@
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- Temp data JSON -->
+    <?php
+    $img_json = json_decode(bloginfo('template_directory') . 'datasource.json' , true);
+    ?>
+
     <!-- Bookmark / Favicons -->
     <link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_directory'); ?>/img/icons/bookmarks/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('template_directory'); ?>/img/icons/bookmarks/apple-icon-60x60.png">
