fea281deaa120c9a21bbdc10d237c464db6abc9b
grantgold <grantagold@gmail.com>
2015-04-13 22:50:29 -0400

    adds open graph type option

--- inc/open-graph.php
+++ inc/open-graph.php
@@ -1,15 +1,16 @@
 <!-- Open Graph -->
-  <meta property="og:type" content="website" />
   <meta property="og:locale" content="en_US" />
   <meta property="og:site_name" content="The MAD Feed" />
 <?php if (is_home()) { ?>
+  <meta property="og:type" content="website" />
   <meta property="og:title" content="<?php echo wp_title(); ?>" />
   <meta property="og:description" content="MAD is a not-for-profit organization that works to expand knowledge of food to make every meal a better meal." />
   <meta property="og:url" content="<?php echo get_site_url(); ?>" />
   <meta property="og:image" content="http://www.madfeed.co/wp-content/themes/madfeed/img/illo/social.jpg"/>
 <?php } else { ?>
+  <meta property="og:type" content="article" />
   <meta property="og:title" content="<?php the_title(); ?>" />
-  <meta property="article:author" content="<?php esc_attr( madfeed_contributor_name()) ?>" />
+  <meta property="article:author" content="<?php echo esc_attr( madfeed_contributor_name()) ?>" />
   <meta property="og:description" content="<?php the_excerpt(); ?>" />
   <meta property="og:url" content="<?php the_permalink(); ?>" />
   <meta property="og:image" content="<?php madfeed_get_social_image(); ?>" />
