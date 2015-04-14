e590cb38bd7f34602a8f1dd39d8a7d88a65d63df
grantgold <grantagold@gmail.com>
2015-04-13 22:46:09 -0400

    More open graph fixes

--- inc/open-graph.php
+++ inc/open-graph.php
@@ -3,9 +3,9 @@
   <meta property="og:locale" content="en_US" />
   <meta property="og:site_name" content="The MAD Feed" />
 <?php if (is_home()) { ?>
-  <meta property="og:title" content="<?php wp_title(); ?>" />
+  <meta property="og:title" content="<?php echo wp_title(); ?>" />
   <meta property="og:description" content="MAD is a not-for-profit organization that works to expand knowledge of food to make every meal a better meal." />
-  <meta property="og:url" content="<?php get_site_url(); ?>" />
+  <meta property="og:url" content="<?php echo get_site_url(); ?>" />
   <meta property="og:image" content="http://www.madfeed.co/wp-content/themes/madfeed/img/illo/social.jpg"/>
 <?php } else { ?>
   <meta property="og:title" content="<?php the_title(); ?>" />
@@ -20,7 +20,7 @@
   <meta name="twitter:site" content="@theMADFeed">
   <meta name="twitter:creator" content="@theMADFeed">
 <?php if (is_home()) { ?>
-  <meta name="twitter:title" content="<?php wp_title(); ?>">
+  <meta name="twitter:title" content="<?php echo wp_title(); ?>">
   <meta name="twitter:description" content="MAD is a not-for-profit organization that works to expand knowledge of food to make every meal a better meal.">
   <meta name="twitter:image:src" content="http://www.madfeed.co/wp-content/themes/madfeed/img/illo/social.jpg" />
 <?php } else { ?>
