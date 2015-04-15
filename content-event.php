580bb68f3f34ac80050c588d9d86243c35e3cbd0
grantgold <grantagold@gmail.com>
2015-04-15 15:22:22 -0400

    Adds LIVE streaming capability
    Using the category "streaming" admins and editors can now change the featured image or video for an event into a live stream capable page. First an embed iframe is needed on a one-off basis before selecting the streaming category.

--- content-event.php
+++ content-event.php
@@ -1,6 +1,21 @@
 <article>
   <div class="container">
-    <?php madfeed_get_featured_image('large'); ?>
+    <?php 
+
+    if (in_category('streaming')) { ?>
+      <div class="feature-image text-center">
+        <iframe src="http://cdnapi.kaltura.com/p/361531/sp/36153100/embedIframeJs/uiconf_id/28822851/partner_id/361531?iframeembed=true&playerId=kaltura_player_1428503147&entry_id=1_5gmgrhel&flashvars[streamerType]=auto" width="640" height="390" allowfullscreen webkitallowfullscreen mozAllowFullScreen frameborder="0" style="width: 800px; height: 480px; margin-top: 50px;" itemprop="video" itemscope itemtype="http://schema.org/VideoObject">
+          <span itemprop="name" content="MHstream 9"></span>
+          <span itemprop="description" content=""></span>
+          <span itemprop="duration" content="0"></span>
+          <span itemprop="thumbnail" content="https://cdnsecakmi.kaltura.com/p/361531/sp/36153100/thumbnail/entry_id/1_5gmgrhel/version/0"></span>
+          <span itemprop="width" content="640"></span>
+          <span itemprop="height" content="390"></span>
+        </iframe>
+      </div>
+    <?php } else {
+      madfeed_get_featured_image('large');
+    } ?>
   </div>
   <div class="container">
       <div id="event-card" class="col-xs-12">
