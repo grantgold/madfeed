8d61199bd25576deece3bbc35610f62439fb5915
Grant Gold <grantgold@Grants-Air.home>
2015-06-29 08:38:03 -0400

    Adds new donate button to About page.

--- page-about.php
+++ page-about.php
@@ -17,15 +17,10 @@
             <br>
             <br>
             <div class="byline">
-              If you would like to help support us by making an online donation, please use our Paypal link here:
-              <br>
-              <br>
-              <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                <input type="hidden" name="cmd" value="_s-xclick">
-                <input type="hidden" name="hosted_button_id" value="5YDWVJ8FJHS8E">
-                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-                <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-              </form>
+              If you would like to help support us by making an online donation, please use the button below:
+              <div class="medium-top-btm-padding">
+                <a class="btn btn-primary" href="https://mad-gsrxi.formstack.com/forms/donate">Make a Donation <i class="fa fa-angle-right"></i></a>
+              </div>
             </div>
           </div>
         </div>  
