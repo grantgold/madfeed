d58d4c99d64a84a6bd00974f00260ede39087424
Grant <grantagold@gmail.com>
2015-10-31 13:49:23 -0400

    Adds template for initiatives

---
+++ page-initiative.php
@@ -0,0 +1,19 @@
+<?php 
+/* Template Name: Initiative */
+?>
+<?php include('header.php'); ?>
+<?php include( INC . 'navbar-content.php' ); ?>
+<div class="row large-top-btm-padding hidden-xs">
+</div>
+<div class="container fadein">
+	<?php loop(); ?>
+</div>
+<section>
+	<div class="row">
+
+		<?php include( INC . 'about-summary.php' ); ?>
+
+	</div>
+</section>
+
+<?php include('footer.php'); ?>
